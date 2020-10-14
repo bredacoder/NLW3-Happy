8a787349e44a2326cafb4b1b1deae52905a61251
Daniel Felipe Developer <danielfelipedeveloper@gmail.com>
2020-10-14 16:44:19 -0400

    Fix: Orphanage View and Delete Database

--- backend/src/views/orphanages_view.ts
+++ backend/src/views/orphanages_view.ts
@@ -4,9 +4,10 @@ import imagesView from './images_view';
 export default {
   render(orphanage: Orphanage ) {
     return {
+      id: orphanage.id,
       name: orphanage.name,
       latitude: orphanage.latitude,
-      longitude: orphanage.latitude,
+      longitude: orphanage.longitude,
       about: orphanage.about,
       instructions: orphanage.instructions,
       opening_hours: orphanage.opening_hours,
