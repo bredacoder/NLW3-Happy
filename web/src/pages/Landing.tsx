abeac13b326c5c90d537d10d71f1e662a98414de
DanielDeveloper <danielfelipedeveloper@gmail.com>
2020-10-21 21:17:02 -0400

    Button Link to Dashboard Login

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -11,16 +11,20 @@ function Landing() {
     <div id="page-landing">
       <div className="content-wrapper">
         <img src={logoImg} alt="Happy Logo"/>
-      
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
-        </main>
 
         <div className="location">
           <strong>Primavera do Leste</strong>
           <span>Mato Grosso</span>
         </div>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <Link to="/login" className="enter-login">
+         Acesso Restrito 
+        </Link>
 
         <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
